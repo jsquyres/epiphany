11ac721bf3c46a54aee33d9dc751521a68bf9ef9
Jeff Squyres <jeff@squyres.com>
2021-08-22 16:05:59 -0400

    wip testing
    Signed-off-by: Jeff Squyres <jeff@squyres.com>

---
+++ pds-queries/2022-stewardship/php/code.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<?php include("google_analytics.inc"); ?>
+</head>
+<body>
+<div align="center"><img src="stewardship-logo.png"></div>
+
+<form action="submit.php">
+<p align="center">Enter your 6-digit Epiphany stewardship code here:</p>
+<input type="text" name="key" id="key" maxlength="6">
+<input type="submit">
+</form>
+</body>
+</html>
