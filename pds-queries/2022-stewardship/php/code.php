4a0bc0e5883eba671c99d209dfb59157a2b6263e
Jeff Squyres <jeff@squyres.com>
2021-08-22 17:51:55 -0400

    wip
    Signed-off-by: Jeff Squyres <jeff@squyres.com>

--- pds-queries/2022-stewardship/php/code.php
+++ pds-queries/2022-stewardship/php/code.php
@@ -8,7 +8,7 @@
 <form action="." method="get">
 <p align="center" style="font-size:18px;">Enter your 6-digit Epiphany stewardship code here:</p>
 <div align="center" >
-<input type="text" name="key" style="font-size:36px;" size="6" maxlength="6">
+<input type="text" name="key" style="font-size:36px;" size="7" maxlength="6">
      <br />
      <br />
 <input style="font-size:18px;" type="submit">
